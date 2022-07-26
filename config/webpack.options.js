c2600ad5311adcb3e7785b0f6d69da3e3a803139
Le Khac Man <lekhacman@outlook.com>
2022-07-27 04:23:03 +0800

    ManifestPlugin

--- config/webpack.options.js
+++ config/webpack.options.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = function optionsPage(env) {
   const devConfig = merge(commonConfig.dev, {
@@ -9,22 +8,8 @@ module.exports = function optionsPage(env) {
       port: 6500,
     },
   });
-  const prodConfig = merge(commonConfig.prod, {
-    plugins: [
-      new WebpackManifestPlugin({
-        generate: function (_, files) {
-          return {
-            action: {
-              default_popup: files.filter((f) => f.name.endsWith('.html'))[0]
-                .name,
-            },
-          };
-        },
-      }),
-    ],
-  });
 
-  return merge(env.WEBPACK_SERVE ? devConfig : prodConfig, {
+  return merge(env.WEBPACK_SERVE ? devConfig : commonConfig.prod, {
     name: 'optionsPage',
     entry: { options: './src/options/options.js' },
     plugins: [
