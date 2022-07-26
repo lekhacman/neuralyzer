6ff9873ea8a5b0813b3a463c28782f799ef850d7
Le Khac Man <lekhacman@outlook.com>
2022-07-27 04:44:29 +0800

    Add pipeline for clients

--- config/webpack.contentScripts.js
+++ config/webpack.contentScripts.js
@@ -3,13 +3,6 @@ const commonConfig = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('./ManifestPlugin');
 
-const contentScripts = {
-  neuralyzer: {
-    path: './src/contentScripts/neuralyzer.js',
-    seed: { matches: ['*://*/*'] },
-  },
-};
-
 module.exports = function neuralyzerConfig(env) {
   const devConfig = merge(commonConfig.dev, {
     plugins: [new HtmlWebpackPlugin({ title: 'Neuralyzer' })],
@@ -23,14 +16,7 @@ module.exports = function neuralyzerConfig(env) {
 
   return merge(env.WEBPACK_SERVE ? devConfig : prodConfig, {
     name: 'neuralyzerConfig',
-    entry: Object.entries(contentScripts).reduce(function (
-      scripts,
-      [key, { path }]
-    ) {
-      scripts[key] = path;
-      return scripts;
-    },
-    {}),
+    entry: { neuralyzer: './src/contentScripts/neuralyzer.js' },
     output: {
       publicPath: '',
     },
